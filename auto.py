df2fa5a4616f8dedae17f3e07cb13d95f7ca6a98
Denis <denis.gerashchenko@testmatick.com>
2019-06-18 05:07:40 +0300

    simple version done

--- auto.py
+++ auto.py
@@ -8,21 +8,10 @@ my_parser.add_argument('Message',
                        type=str,
                        help='the commit message')
 
-my_parser.add_argument('Password',
-                       metavar='message',
-                       type=str,
-                       help='the password')
 
 args = my_parser.parse_args()
 message = args.Message
-password = args.Password
-login = 'Denis-Gerashchenko'
-
-encoded_login = bytes(login, 'utf-8')
-encoded_password = bytes(password, 'utf-8')
 
 subprocess.call(['git', 'add', '-A'])
 subprocess.call(['git', 'commit', '-m', f'{message}'])
-proc = subprocess.Popen(['git', 'push', '-u', 'origin', 'master'], stdin=subprocess.PIPE,stdout=subprocess.PIPE)
-proc.stdin.write(encoded_login)
-proc.stdin.write(encoded_password)
+subprocess.call(['git', 'push', '-u', 'origin', 'master'])
